c1b11427a090c06f6db737648b03962a6e866f96
Jonathan Fine <jfine2358@gmail.com>
2017-07-31 11:27:24 +0100

    Start adding features to BytesDessin.

--- py/dessins/work.py
+++ py/dessins/work.py
@@ -4,4 +4,12 @@ class BytesDessin(tuple):
 
     def __new__(cls, alpha, beta):
 
+        if len(alpha) != len(beta):
+            raise ValueError
+
         return tuple.__new__(cls, (alpha, beta))
+
+
+    def __len__(self):
+
+        return len(self[0])
--- py/tests/test_work.py
+++ py/tests/test_work.py
@@ -1,6 +1,13 @@
 '''Test work file for dessins'''
+import pytest
 
 from dessins.work import BytesDessin
 
 def test_create():
-    BytesDessin(bytes(range(4)), bytes(range(4)))
+
+    d = BytesDessin(bytes(range(4)), bytes(range(4)))
+
+    assert len(d) == 4
+
+    with pytest.raises(ValueError):
+        BytesDessin(bytes(range(3)), bytes(range(4)))
