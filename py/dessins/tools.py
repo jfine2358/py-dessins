a43308c78efd91db2824a1ccac13becb2b580451
Jonathan Fine <jfine2358@gmail.com>
2017-08-02 07:55:46 +0100

    Add tools.is_perm(seq) - with perhaps too much testing.

--- py/dessins/tools.py
+++ py/dessins/tools.py
@@ -1,3 +1,20 @@
 '''Tools for dessins
 
 '''
+
+import collections
+
+def is_perm(seq):
+    '''Return True if seq is permutation of {0, ..., n}.
+
+    >>> is_perm([0,1]) and is_perm([1, 0]) and is_perm([1, 2, 0])
+    True
+    >>> not is_perm([1]) and not is_perm([2, 0, 0])
+    True
+    '''
+    if not isinstance(seq, collections.Sequence):
+        return False
+
+    value = list(seq)
+    value.sort()
+    return value == list(range(len(value)))
--- py/tests/test_tools.py
+++ py/tests/test_tools.py
@@ -2,6 +2,38 @@
 
 '''
 
+import itertools
+import pytest
+
 def test_import():
 
     import dessins.tools
+
+
+def test_is_perm():
+    from dessins.tools import is_perm
+
+    # Every itertools permutation passes.
+    for i in range(5):
+        for item in itertools.permutations(range(i)):
+            # Whatever the sequence type.
+            for t in list, tuple, bytes:
+                assert is_perm(t(item)), (t, item)
+
+    # But non-permutations of {0, ..., n} fail.
+    assert not is_perm([1])
+    assert not is_perm([1, 1])
+
+    # Every suitable sequence of ints passes.
+    for item in (
+            list(range(4)),
+            bytes(range(4)),
+    ):
+        assert is_perm(item), item
+
+    # Empty iterables fail, if not a sequence.
+    assert not is_perm(dict())
+    assert not is_perm(set())
+
+    # Empty strings pass!
+    assert is_perm(str())
