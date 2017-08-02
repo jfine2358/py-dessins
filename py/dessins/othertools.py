9784c6f344b865308a3219939e266bfc8d537f8c
Jonathan Fine <jfine2358@gmail.com>
2017-08-02 16:32:20 +0100

    Add bytes_from_str62(s) and str62_from_bytes(seq).

--- py/dessins/othertools.py
+++ py/dessins/othertools.py
@@ -1,1 +1,45 @@
 '''Other tools'''
+
+import string
+
+BASE62_CHARS = string.digits + string.ascii_letters
+BASE62_LOOKUP = dict(zip(BASE62_CHARS, range(len(BASE62_CHARS))))
+def bytes_from_str62(s):
+    '''Return base 62 conversion of string to bytes.
+
+    >>> seq = bytes_from_str62('09azAZ')
+    >>> tuple(seq)
+    (0, 9, 10, 35, 36, 61)
+
+    >>> type(seq) == bytes
+    True
+
+    >>> bytes_from_str62('abc$')
+    Traceback (most recent call last):
+    ValueError: 'abc$'
+    '''
+    try:
+        return bytes(BASE62_LOOKUP[i] for i in s)
+    except KeyError:
+        raise ValueError(repr(s))
+
+
+def str62_from_bytes(seq):
+    '''Return base 62 conversion of bytes to string.
+
+    >>> seq = bytes_from_str62('09azAZ')
+    >>> str62_from_bytes(seq)
+    '09azAZ'
+
+    >>> str62_from_bytes(bytes([62]))
+    Traceback (most recent call last):
+    ValueError: b'>'
+
+    GOTCHA: TODO: Fix this negative indexing problem.
+    >>> str62_from_bytes([-1])
+    'Z'
+    '''
+    try:
+        return ''.join(BASE62_CHARS[i] for i in seq)
+    except IndexError:
+        raise ValueError(repr(seq))
--- py/tests/test_othertools.py
+++ py/tests/test_othertools.py
@@ -4,3 +4,12 @@
 def test_imports():
 
     import dessins.othertools
+    from dessins.othertools import bytes_from_str62
+    from dessins.othertools import str62_from_bytes
+
+
+def test_bytes_to_and_from_str62():
+
+    from dessins.othertools import bytes_from_str62
+    from dessins.othertools import str62_from_bytes
+    # TODO: Provide additional tests.
