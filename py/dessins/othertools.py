f55341479aae5c4bba693c1c8ea059cb5963ccc8
Jonathan Fine <jfine2358@gmail.com>
2017-08-03 11:35:02 +0100

    Add othertools.SetDiff().

--- py/dessins/othertools.py
+++ py/dessins/othertools.py
@@ -43,3 +43,63 @@ def str62_from_bytes(seq):
         return ''.join(BASE62_CHARS[i] for i in seq)
     except IndexError:
         raise ValueError(repr(seq))
+
+
+class SetDiff(tuple):
+    '''Record and update difference between two sets
+
+    Think of this as a double-pan balance scale for sets.
+
+    The basic operation is adding elements to one side or the
+    other. If the same element is added first to one side and then the
+    other, it removes (cancels) the element on the first side.
+
+    >>> diff = SetDiff()
+    >>> diff
+    (set(), set())
+
+    We can add an element to one side of the other.
+    >>> diff.add(0, 'a'); diff
+    ({'a'}, set())
+    >>> diff.add(1, 'b'); diff
+    ({'a'}, {'b'})
+
+    It's an error to add an element to a side that already has it.
+    >>> diff.add(0, 'a')
+    Traceback (most recent call last):
+    ValueError
+
+    If element already in diff, add to other side to remove from diff.
+    >>> diff.add(1, 'a'); diff
+    (set(), {'b'})
+
+    >>> diff.add(0, 'b'); diff
+    (set(), set())
+    '''
+
+    def __new__(cls):
+
+        return tuple.__new__(cls, (set(), set()))
+
+
+    def add(self, i, elt):
+        '''Add elt to side i (or remove from other side).'''
+
+        self.update(i, {elt})
+
+
+    def update(self, i, elts):
+        '''Add elts to side i (or remove from other side).'''
+
+        if i == 0:
+            tgt, other = self
+        elif i == 1:
+            other, tgt = self
+        else:
+            raise ValueError
+
+        if not elts.isdisjoint(tgt):
+            raise ValueError
+
+        tgt.update(elts.difference(other))
+        other.difference_update(elts)
--- py/tests/test_othertools.py
+++ py/tests/test_othertools.py
@@ -6,10 +6,17 @@ def test_imports():
     import dessins.othertools
     from dessins.othertools import bytes_from_str62
     from dessins.othertools import str62_from_bytes
+    from dessins.othertools import SetDiff
 
 
 def test_bytes_to_and_from_str62():
 
     from dessins.othertools import bytes_from_str62
     from dessins.othertools import str62_from_bytes
+    # TODO: Provide additional tests.
+
+
+def test_SetDiff():
+
+    from dessins.othertools import SetDiff
     # TODO: Provide additional tests.
