e36bd10d6eaf492a29861dfa8d68892a0230e3df
Jonathan Fine <jfine2358@gmail.com>
2017-08-12 15:58:46 +0100

    Make a start in discovering terms in products and their best relabellings.

--- py/dessins/computations.py
+++ py/dessins/computations.py
@@ -42,6 +42,37 @@ The product has two terms.
 >>> [len(tuple(BB.iter_relabel(i)))//2 for i in range(7)]
 [7, 42, 42, 42, 42, 42, 42]
 
+Let's discover the terms, and their best relabellings.
+>>> for t, i in sorted(every_relabel_of(AA))[:5]: print(len(t)//2, i, t[:10])
+7 0 (1, 0, 0, 2, 3, 1, 4, 3, 2, 5)
+7 48 (1, 0, 0, 2, 3, 1, 4, 5, 2, 4)
+42 6 (1, 0, 0, 2, 3, 1, 4, 5, 2, 6)
+42 42 (1, 0, 0, 2, 3, 1, 4, 5, 2, 6)
+7 24 (1, 0, 2, 3, 0, 4, 5, 1, 6, 2)
+
+>>> for t, i in sorted(every_relabel_of(BB))[:5]: print(len(t)//2, i, t[:10])
+7 0 (1, 0, 0, 2, 3, 1, 2, 4, 5, 3)
+7 40 (1, 0, 2, 1, 0, 3, 4, 2, 3, 5)
+7 48 (1, 0, 2, 3, 0, 2, 4, 1, 3, 5)
+42 41 (1, 0, 2, 3, 0, 4, 5, 1, 6, 2)
+42 47 (1, 0, 2, 3, 0, 4, 5, 1, 6, 2)
+
+>>> for t, i in sorted(every_relabel_of(AB))[:5]: print(len(t)//2, i, t[:10])
+49 42 (1, 0, 0, 2, 3, 1, 4, 5, 6, 7)
+49 0 (1, 0, 0, 2, 3, 1, 4, 5, 6, 7)
+49 27 (1, 0, 2, 3, 0, 4, 5, 1, 6, 2)
+49 26 (1, 0, 2, 3, 0, 4, 5, 1, 6, 2)
+49 6 (1, 0, 2, 3, 4, 2, 5, 1, 6, 7)
+
+Now use the information we've obtained.
+>>> t = tuple(AA_0.iter_relabel(0)); (len(t)//2, t[:20])
+(42, (1, 0, 0, 2, 3, 1, 4, 5, 2, 6, 7, 3, 8, 4, 9, 10, 11, 12, 13, 14))
+
+>>> t = tuple(BB_0.iter_relabel(0)); (len(t)//2, t[:20])
+(42, (1, 0, 2, 3, 0, 4, 5, 1, 6, 2, 7, 8, 9, 10, 11, 12, 13, 5, 14, 15))
+
+>>> t = tuple(AB_0.iter_relabel(0)); (len(t)//2, t[:20])
+(49, (1, 0, 0, 2, 3, 1, 4, 5, 6, 7, 8, 3, 9, 10, 11, 4, 12, 8, 13, 14))
 '''
 
 from .work import A4 as A_orig
@@ -63,7 +94,6 @@ AA = A * A
 AB = A * B
 BB = B * B
 
-
 def every_relabel_of(permpair):
 
     value = []
@@ -72,3 +102,11 @@ def every_relabel_of(permpair):
         value.append((ints, i))
 
     return value
+
+
+# As dessins, AA = A + AA_0.
+# As dessins, BB = B + BB_0.
+# As dessins, AB = AB_0
+AA_0 = permpair_from_iterable(AA.iter_relabel(6))
+BB_0 = permpair_from_iterable(BB.iter_relabel(41))
+AB_0 = permpair_from_iterable(AB.iter_relabel(42))
