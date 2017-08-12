b26a88146d531c3d359da016dc1db1d1c7875c34
Jonathan Fine <jfine2358@gmail.com>
2017-08-12 11:46:47 +0100

    Add work.PermPair, with __mul__ method.

--- py/dessins/work.py
+++ py/dessins/work.py
@@ -25,18 +25,78 @@ def bytes_from_ints(typecode, ints):
     return array(typecode, ints).tobytes()
 
 A2 = bytes_from_ints('L', A1)
+B2 = bytes_from_ints('L', B1)
 
 
 def aaa_from_bytes(bindata):
     '''
-    >>> alpha, beta = aaa_from_bytes(A2)
-    >>> tuple(map(alpha, range(7)))
+    >>> alpha, beta, length= aaa_from_bytes(A2)
+    >>> tuple(map(alpha, range(length)))
     (1, 2, 0, 5, 6, 3, 4)
-    >>> tuple(map(beta, range(7)))
+    >>> tuple(map(beta, range(length)))
     (0, 3, 4, 1, 2, 5, 6)
     '''
 
     ulongs = memoryview(bindata).cast('L')
+    length = len(ulongs[::2])
     alpha = ulongs[::2].__getitem__
     beta = ulongs[1::2].__getitem__
-    return alpha, beta
+    return alpha, beta, length
+
+
+A3 = aaa_from_bytes(A2)
+B3 = aaa_from_bytes(B2)
+
+
+class PermPair:
+    '''
+    >>> tuple(map(A4.alpha, range(len(A4))))
+    (1, 2, 0, 5, 6, 3, 4)
+    >>> tuple(map(A4.beta, range(len(A4))))
+    (0, 3, 4, 1, 2, 5, 6)
+
+    >>> A4_A4 = A4 * A4
+    >>> tuple(map(A4_A4.alpha, range(14)))
+    (8, 9, 7, 12, 13, 10, 11, 15, 16, 14, 19, 20, 17, 18)
+
+    >>> A4_B4 = A4 * B4
+    >>> tuple(map(A4_B4.alpha, range(14)))
+    (8, 9, 7, 11, 10, 13, 12, 15, 16, 14, 18, 17, 20, 19)
+
+    '''
+
+    __slots__ = 'alpha', 'beta', 'length'
+
+    def __init__(self, alpha, beta, length):
+
+        self.alpha = alpha
+        self.beta = beta
+        self.length = length
+
+
+    def __len__(self):
+        return self.length
+
+
+    def __mul__(self, other):
+
+        if not isinstance(other, PermPair):
+            raise ValueError
+
+        length = self.length * other.length
+
+        def alpha(edge):
+
+            i, j = divmod(edge, other.length)
+            return other.length * self.alpha(i) + other.alpha(j)
+
+        def beta(edge):
+
+            i, j = divmod(edge, other.length)
+            return other.length * self.beta(i) + other.beta(j)
+
+        return PermPair(alpha, beta, length)
+
+
+A4 = PermPair(*A3)
+B4 = PermPair(*B3)
